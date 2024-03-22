be520116b7f5846e506fa14dea5fe8f75ed24098
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-03-22 09:06:42 +0700

    feat: testing streaming

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import asyncio
 import os
 import pprint
 
 import markdown2
-import pretty_errors
 from anthropic import Anthropic
+from anthropic import AsyncAnthropic
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ As a holistic Pull Request reviewer, thoroughly evaluate the <following code>, a
     * First try to guess the programming language/framework and the high level purpose of the code
     * Then, validate its against the best practices/conventions and standards of the language/framework
     * Focus on maintainability, readability, simplicity, adaptability
-    * Naming:
+    * Next, check the naming conventions using the following criteria:
         * Understandabiliy: Should describe the concept it designs
         * Conciseness: Should use only the words necessary to describe the concept it represents
         * Consistency: Should be used and formatted uniformly
@@ -31,42 +32,75 @@ As a holistic Pull Request reviewer, thoroughly evaluate the <following code>, a
 """
 
 
-def request_code_review(file_content, model):
-    client = Anthropic(
-        api_key=os.getenv("ANTHROPIC_API_KEY"),
-    )
-    response = client.messages.create(
-        max_tokens=4096,
-        temperature=0.0,
-        system=CODE_REVIEW_PROMPT,
+# def request_code_review(file_content, model):
+#     client = Anthropic(
+#         api_key=os.getenv("ANTHROPIC_API_KEY"),
+#     )
+
+#     def generate():
+#         stream = client.messages.create(
+#             max_tokens=4096,
+#             temperature=0.0,
+#             system=CODE_REVIEW_PROMPT,
+#             messages=[
+#                 {
+#                     "role": "user",
+#                     "content": f"Code review the following file: {file_content}",
+#                 }
+#             ],
+#             model=model,
+#             stream=True,
+#         )
+#         pprint.pprint(dir(stream.response))
+
+#         for text in stream.response.text:
+#             print(text, end="", flush=True)
+
+#     return Response(generate(), mimetype="text/event-stream")
+
+
+# def start_code_review():
+#     """Initiate code review process."""
+#     load_dotenv()
+
+#     model = os.getenv("CLAUDE_MODEL") or "claude-3-haiku-20240307"
+#     filename = os.getenv("FILENAME") or "test.txt"
+
+#     with open(filename, "r") as file:
+#         file_content = file.read()
+
+#     return request_code_review(file_content, model)
+
+
+# @app.route("/")
+# def render_code_review_result():
+#     """Render code review result."""
+#     return start_code_review()
+
+
+client = AsyncAnthropic()
+
+
+async def main() -> None:
+    async with client.messages.stream(
+        max_tokens=1024,
         messages=[
             {
                 "role": "user",
-                "content": f"Code review the following file: {file_content}",
+                "content": "Write a short essay consider at least 3 paragraphs about resilient for startup founder running by one person",
             }
         ],
-        model=model,
-    )
-    pprint.pprint(response)
-    return response.content[0].text
+        model="claude-3-haiku-20240307",
+    ) as stream:
+        async for text in stream.text_stream:
+            print(text, end="", flush=True)
+        print()
+
+    # you can still get the accumulated final message outside of
+    # the context manager, as long as the entire stream was consumed
+    # inside of the context manager
+    accumulated = await stream.get_final_message()
+    print("accumulated message: ", accumulated.model_dump_json(indent=2))
 
 
-def start_code_review():
-    """Initiate code review process."""
-    load_dotenv()
-    # Legacy model is more expensive
-    # Currently the Haiku model is the fastest and cheapest, but not released
-    # yet
-    model = os.getenv("CLAUDE_MODEL") or "claude-3-haiku-20240307"
-    filename = os.getenv("FILENAME") or "test.txt"
-    with open(filename, "r") as file:
-        file_content = file.read()
-    return request_code_review(file_content, model)
-
-
-@app.route("/")
-def render_code_review_result():
-    """Render code review result."""
-    content = start_code_review()
-    result = markdown2.markdown(content)
-    return render_template("code_review.html", result=result)
+asyncio.run(main())
